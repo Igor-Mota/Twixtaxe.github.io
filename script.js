6b02c8d5fc61e0a59ce2dd9ed995a5c9e2ce8ae6
Twixtaxe <46041463+Twixtaxe@users.noreply.github.com>
2020-01-22 10:51:41 -0300

    finalizando o jogo

--- script.js
+++ script.js
@@ -10,7 +10,17 @@ const jogo_da_velha = {
     },
     container_element: null,
     gameover: false,
-
+    sequencias_de_vitoria:[
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ],
+    
     init: function(container){
         this.container_element = container;
     },
@@ -20,9 +30,44 @@ const jogo_da_velha = {
         if(this.board[position] ===""){
             this.board[position] = this.simbols.option[this.simbols.qual_turno];
             this.desenha();
-
-            this.simbols.troca();
+            let alguem_venceu = this.verifica_se_tem_uma_sequecia_vencedora(this.simbols.option[this.simbols.qual_turno]);
+            if(alguem_venceu >=0){
+                
+                this.acabou_o_jogo();
+            
+            }else{
+                this.simbols.troca();
+            }
+            return true;
+            
+        }else{
+            return false;
         }    
+    },
+
+    acabou_o_jogo: function(){
+        this.gameover = true;
+        console.log("game Over" + i )
+    },
+
+    restart: function(){
+        this.btn.addEventListener("click", this.start)
+    },
+    start: function(){
+        this.board.fill("")
+        this.desenha();
+        this.gameover = false;
+    },
+
+    verifica_se_tem_uma_sequecia_vencedora: function(simbol){
+        for(i in this.sequencias_de_vitoria){
+            if(this.board[ this.sequencias_de_vitoria[i][0] ] == simbol &&
+               this.board[ this.sequencias_de_vitoria[i][1] ] == simbol && 
+               this.board[this.sequencias_de_vitoria[i][2] ] == simbol){
+                    return i;
+                }
+        };
+        return -1;
     },
 
     desenha: function(){
@@ -33,4 +78,6 @@ const jogo_da_velha = {
         }
         this.container_element.innerHTML = content;
     },
+
+
 };
