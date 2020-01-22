191a1cc9fc689cedfbc7e909f3fb5a69b59a3af0
Your Name <you@example.com>
2020-01-22 09:30:39 -0300

    adicionando função de desho

---
+++ script.js
@@ -0,0 +1,18 @@
+const jogo_da_velha = {
+    board: ["X","","","","O","","","",""],
+    simbols:["X","O"],
+    container_element: null,
+    
+    init: function(container){
+        this.container_element = container;
+    },
+
+    desenha: function(){
+        let content = "";
+
+        for(i in this.board){
+            content += "<div>" + this.board[i] + "</div>";
+        }
+        this.container_element.innerHTML = content;
+    },
+};
