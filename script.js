6f4cc3c796063eba6643a1df619cae3c94e6e0ee
Twixtaxe <46041463+Twixtaxe@users.noreply.github.com>
2020-01-22 09:56:23 -0300

    adicionando o evento de click

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 const jogo_da_velha = {
-    board: ["X","","","","O","","","",""],
-    simbols:["X","O"],
+    board: ["","","","","","","","",""],
+    simbols:{
+      option:["X","O"],
+      qual_turno: 0,
+      
+      troca: function(){
+        this.qual_turno = (this.qual_turno === 0 ? 1 : 0);
+      }
+    },
     container_element: null,
-    
+    gameover: false,
+
     init: function(container){
         this.container_element = container;
     },
 
-    jogada: function(){
-        console.log("jogada")
+    jogada: function(position){
+        if (this.gameover) return false;
+        if(this.board[position] ===""){
+            this.board[position] = this.simbols.option[this.simbols.qual_turno];
+            this.desenha();
+
+            this.simbols.troca();
+        }    
     },
 
     desenha: function(){
         let content = "";
 
         for(i in this.board){
-            content += "<div>" + this.board[i] + "</div>";
+            content += "<div onCLick='jogo_da_velha.jogada("+ i +")'>" + this.board[i] + "</div>";
         }
         this.container_element.innerHTML = content;
     },
